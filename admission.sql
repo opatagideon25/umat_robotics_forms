c12d0000ec13aa027e876b2f79ac480a1df68cc9
opatagideon25 <opatagideon25@gmail.com>
2024-03-18 12:20:29 +0000

    It updates the content of admission.sql (database)

--- admission.sql
+++ admission.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3308
--- Generation Time: Aug 22, 2023 at 03:02 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Mar 18, 2024 at 01:07 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,47 +18,35 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `admission`
+-- Database: `crud`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `info`
+-- Table structure for table `user_info`
 --
 
-CREATE TABLE `info` (
+CREATE TABLE `user_info` (
   `ID` int(10) NOT NULL,
-  `Degree` varchar(100) NOT NULL,
-  `Title` varchar(100) NOT NULL,
-  `Surname` varchar(100) NOT NULL,
-  `Firstname` varchar(100) NOT NULL,
-  `Middlename` varchar(100) NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `DOB` varchar(100) NOT NULL,
-  `POB` varchar(100) NOT NULL,
-  `Username` varchar(100) NOT NULL,
-  `Password` varchar(100) NOT NULL,
-  `Code` varchar(100) NOT NULL
+  `fullname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `info`
+-- Dumping data for table `user_info`
 --
-
-INSERT INTO `info` (`ID`, `Degree`, `Title`, `Surname`, `Firstname`, `Middlename`, `Email`, `DOB`, `POB`, `Username`, `Password`, `Code`) VALUES
-(2, 'degree', 'Mr', 'Bador', 'Gideon', 'Opata', 'opatagideon25@gmail.com', '2003-04-24', 'Berekum', 'Gideon', 'icui4cuitN#', 'CUCG-001'),
-(3, 'diploma', 'Mr', 'bador ', 'opata', 'gideon', 'opatagideon@gmail.com', '2023-09-06', 'Berekum', 'Gideon', 'icui4cuitN#', 'CUCG-001'),
-(4, 'diploma', 'Mr', 'bador ', 'opata', 'gideon', 'opatagideon@gmail.com', '2023-09-06', 'Berekum', 'Gideon', 'icui4cuitN#', 'CUCG-001');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `info`
+-- Indexes for table `user_info`
 --
-ALTER TABLE `info`
+ALTER TABLE `user_info`
   ADD PRIMARY KEY (`ID`);
 
 --
@@ -66,10 +54,10 @@ ALTER TABLE `info`
 --
 
 --
--- AUTO_INCREMENT for table `info`
+-- AUTO_INCREMENT for table `user_info`
 --
-ALTER TABLE `info`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `user_info`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
